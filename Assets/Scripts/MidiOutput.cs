df4e303ed9579d1735ea08cd0146a829b0decb36
zah8796 <zah8796@gmail.com>
2024-02-26 14:21:41 -0500

    Documentation
    Documentation has been added to the class MidiOutput as well as its functions and integral components.

--- Assets/Scripts/MidiOutput.cs
+++ Assets/Scripts/MidiOutput.cs
@@ -8,6 +8,11 @@ using Melanchall.DryWetMidi.Interaction;
 using System.Text;
 using System.Linq;
 
+/**
+* This class currently governs midi map creation, map tracking, output device, and note creation.
+* A series of Lists are implemented to keep track of notes that have been played, are currently 
+* playing, and will be played as well as displaying them upon being played as a TMP.
+*/
 public class MidiOutput : MonoBehaviour
 {
     private SpriteCreator spriteCreator;
@@ -43,6 +48,7 @@ public class MidiOutput : MonoBehaviour
             Debug.Log("Output Device Found: " + device.Name);
         }
 
+        //Tracks note names/types and assigns an ID value.
         noteLookupTable = new Dictionary<Melanchall.DryWetMidi.MusicTheory.NoteName, int>()
         {
             {Melanchall.DryWetMidi.MusicTheory.NoteName.C, 0},
@@ -77,6 +83,9 @@ public class MidiOutput : MonoBehaviour
         }
     }
 
+    /**
+    * The following function clears and releases the midi player upon closing the application.
+    */
     void OnApplicationQuit() {
         if (playback != null) {
             playback.NotesPlaybackStarted -= OnNotesPlaybackStarted;
@@ -91,11 +100,16 @@ public class MidiOutput : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Sets the TMP element on screen to display the names of the notes being played.
         if (!notesPlaying.Equals(lastPlayed)) {
             noteLogger.text = notesPlaying;
             lastPlayed = notesPlaying;
         }
 
+        /*
+        * Each update any notes in the notesToPlayOnUpdate List will be generated onto the screen
+        * with an assigned note prefab object.
+        */
         if (notesToPlayOnUpdate.Count != 0) {
             foreach (int value in notesToPlayOnUpdate) {
                 spriteCreator.generateNote(value);
@@ -104,10 +118,15 @@ public class MidiOutput : MonoBehaviour
             notesToPlayOnUpdate.Clear();
         }
 
+        /*
+        * The following logical chain starts, stops, and resets midi playback using
+        * the spacebar
+        */
         if (Time.time > timestamp + 0.50f) {
             if (Input.GetKey(KeyCode.Space)) {
                 timestamp = Time.time;
 
+                //These assignments clear the note display TMPs
                 notesPlaying = "";
                 lastPlayed = "";
                 noteLogger.text = "";
@@ -122,9 +141,14 @@ public class MidiOutput : MonoBehaviour
         }
     }
 
+    /**
+    * The following function governs the setup of midi playback in the system.
+    * It sets the track timing, note TMP, and the List containing note order and details.
+    */
     private void OnNotesPlaybackStarted(object sender, NotesEventArgs e)
     {
         if (e.Notes.ElementAt(0) != null) {
+            //Gets the midi timestamp of the first note
             long time = e.Notes.ElementAt(0).Time;
 
             if (time != lastTimeParsed) {
@@ -141,6 +165,10 @@ public class MidiOutput : MonoBehaviour
 
                         int noteID;
 
+                        /*
+                        * Checks whether the current note exists within the lookup table and if it is not currently
+                        * in the List for notes to play, it is added by ID.
+                        */
                         if (noteLookupTable.TryGetValue(current.NoteName, out noteID)) {
                             if (!notesToPlayOnUpdate.Contains(noteID)) {
                                 notesToPlayOnUpdate.Add(noteID);
@@ -151,6 +179,7 @@ public class MidiOutput : MonoBehaviour
                     Debug.Log("time missing - this shouldnt happen");
                 }
 
+                //This removes whitespace and note accidentals from the current note string for the TMP.
                 notesPlaying = notesPlaying.Trim().Replace("Sharp", "#");
 
                 //Debug.Log("Time " + time + " (" + notesPlaying + ")");
