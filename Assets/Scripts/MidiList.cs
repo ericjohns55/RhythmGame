57c593cd416b95a91c3586ee9ab3ad6b34e45347
v-brouse <148996319+v-brouse@users.noreply.github.com>
2024-03-22 12:28:58 -0400

    Worked on MidiHandler Screen
    Made a new script called MidiList.cs to replace MidiHandlerFolder.cs. Worked on adding UIs to the MidiHandler Canvas.

---
+++ Assets/Scripts/MidiList.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.IO;
+using System.IO;
+
+public class MidiList : MonoBehaviour
+{
+    public string midiFolderPath = "Assets/MIDIs";
+    public GameObject buttonPrefab;
+    public Transform contentPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadMidiFiles();
+    }
+
+    void LoadMidiFiles()
+    {
+        string[] midiFiles = Directory.GetFiles(Application.dataPath + "/" + midiFolderPath, "*.mid");
+
+        foreach (string midiFile in midiFiles) {
+            string fileName = Path.GetFileNameWithoutExtension(midiFile);
+            GameObject button = Instantiate(buttonPrefab, contentPanel);
+            button.GetComponentInChildren<Text>().text = fileName;
+
+            button.GetComponent<Button>().onClick.AddListener(() => LoadMidiFile(midiFile));
+        }
+    }
+
+    void LoadMidiFile(string filePath)
+    {
+        // Load the MIDI file
+        MidiFile midiFile = MidiFile.Read(filePath);
+
+    }
+}
