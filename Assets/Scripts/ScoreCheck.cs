340a05e66441e9dc8fe59356879eb51fd4f56ebb
zah8796 <zah8796@gmail.com>
2024-03-25 14:44:16 -0400

    Logic placement and key detection
    As of now, the ScoreCheck script should find the keys being pressed at a given time and compare them against the keys being used for the game. Then said keys are checked as to whether or not they match a note to be hit at that time and if so, calculate the necessary scoring accordingly.

--- Assets/Scripts/ScoreCheck.cs
+++ Assets/Scripts/ScoreCheck.cs
@@ -2,29 +2,87 @@ using System.Collections;
 using System.Collections.Generic;
 using Melanchall.DryWetMidi.Interaction;
 using UnityEngine;
+using UnityEngine.UIElements;
+using System.Windows.Input;
+using System;
 
 public class ScoreCheck : MonoBehaviour
 {
     float noteTime;
+    float pressTime;
     int noteID;
+    int noteRetrieve;
+    float latency;
 
-    List<KeyCode> keys = new List<KeyCode>() {
-        KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.F,
-        KeyCode.J, KeyCode.K, KeyCode.L, KeyCode.Semicolon
+    Dictionary<KeyCode, int> keys = new Dictionary<KeyCode, int>(){
+        {KeyCode.A, 1}, {KeyCode.S, 2}, {KeyCode.D, 3}, {KeyCode.F, 4}, {KeyCode.J, 5},
+        {KeyCode.K, 6}, {KeyCode.L, 7}, {KeyCode.Semicolon, 8}
     };
 
 
     // Start is called before the first frame update
     void Start()
     {
-        float latency = PlayerPrefs.GetFloat("Total_Latency");
+        latency = PlayerPrefs.GetFloat("Total_Latency");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(keys.Exists)
+        // Left off here. Need to match the note being played to the proper key then set 
+        // up calculation logic of scoring range. Note offset in game must be tweaked
+        foreach(KeyValuePair<KeyCode, int> key in keys)
+        {
+            if(Input.anyKey)
+            {
+                 foreach (KeyCode keyCode in System.Enum.GetValues(typeof(KeyCode)))
+                {  
+                    if(keys.ContainsKey(keyCode))
+                    {
+                        keys.TryGetValue(keyCode, out noteRetrieve);
+
+                        if(noteRetrieve == noteID)
+                        {
+                            pressTime = Time.time;
+                            ScoreRegister(pressTime);
+                        }
+                    }
+                }   
+            }
+        }
+    }
+
+    public void ScoreRegister(float presstime)
+    {
+        float range;
+
+        if(latency > 0)
+        {
+            range = noteTime - (presstime - latency);
+        }
+        else
+        {
+            range = noteTime - (presstime + latency);
+        }
+
+        float absRange = Math.Abs(range);
+
+        if(absRange < 0.2) //perfect
+        {
+
+        }
+        else if(absRange < 0.5) //good
+        {
+            
+        }
+        else if(absRange < 0.7) //bad
+        {
+
+        }
+        else //miss
+        {
+
+        }
     }
 
     public void SetNoteTime(float time)
@@ -32,6 +90,8 @@ public class ScoreCheck : MonoBehaviour
         noteTime = time;
     }
 
+
+    //this may not be necessary
     public void SetNoteID(int id)
     {
         noteID = id;
