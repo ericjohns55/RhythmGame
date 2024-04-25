71db0defe321412e6b28f6d9bf6f067121f9cfd7
Eric Johns <eric275j@gmail.com>
2024-04-25 18:33:46 -0400

    Added checkbox for ghost notes

--- Assets/Scripts/DifficultySelector.cs
+++ Assets/Scripts/DifficultySelector.cs
@@ -6,14 +6,17 @@ public class DifficultySelector : MonoBehaviour
     public Toggle easyToggle;
     public Toggle mediumToggle;
     public Toggle hardToggle;
+    public Toggle ghostToggle;
 
     private const string DifficultyKey = "SelectedDifficulty";
+    private const string GhostKey = "GhostNotes";
 
     public void Start()
     {
         easyToggle.onValueChanged.AddListener(delegate { OnToggleActivated(easyToggle, Difficulty.Easy); });
         mediumToggle.onValueChanged.AddListener(delegate { OnToggleActivated(mediumToggle, Difficulty.Medium); });
         hardToggle.onValueChanged.AddListener(delegate { OnToggleActivated(hardToggle, Difficulty.Hard); });
+        ghostToggle.onValueChanged.AddListener(delegate { ToggleGhostNotes(ghostToggle); });
     }
 
     public void OnToggleActivated(Toggle toggle, Difficulty difficulty)
@@ -25,6 +28,15 @@ public class DifficultySelector : MonoBehaviour
 
             Debug.Log("Selected difficulty: " + difficulty.ToString());
         }
+    }
+
+    public void ToggleGhostNotes(Toggle toggle) {
+        bool ghostNotesStatus = toggle.isOn;
+
+        PlayerPrefs.SetString(GhostKey, ghostNotesStatus.ToString());
+        PlayerPrefs.Save();
+
+        Debug.LogFormat("Ghost notes: {0}", ghostNotesStatus ? "enabled" : "disabled");
     }
 }
 
